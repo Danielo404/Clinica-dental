b310526c850694cc4700023a221e1e20e3a7ebb9
Asklat <57771727+Asklat@users.noreply.github.com>
2021-01-22 15:16:22 +0100

    nueva web

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
+    var altura = $('.menu').offset().top;
+
     $(window).on('scroll', function(){
 
-        if( $(window).scrollTop() > 0)
+        if( $(window).scrollTop() > altura)
         {
             $('.menu').addClass('menu-fixed');
+            $('.contenedor-body').addClass('paddin');
         } 
         else
         {
             $('.menu').removeClass('menu-fixed');
+            $('.contenedor-body').removeClass('paddin');
         }
     });
 });
